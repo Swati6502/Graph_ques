93966e8c63f80306d6ef7c7236c471e3cc039589
Swati6502 <joshiswati739@gmail.com>
2025-06-24 23:52:37 +0530

    add comments in bfs code

--- bfs_graph.cpp
+++ bfs_graph.cpp
@@ -9,20 +9,28 @@
 
 void bfs(unordered_map<int,list<int>> &adj,unordered_map<int,bool> &visited,vector<int>&ans, int node){
 
+// bfs =  check node and then its adjacent nodes
+// use queue because easily push from back and pop from front
      queue<int>q;
 
+// pushing first node to queue
      q.push(node);
+
+    //  make it true in visited 
      visited[node] = true;
+
+    //  travers till the time queue is not empty
      while(!q.empty()){
         int front = q.front();
         q.pop();
 
         ans.push_back(front);
 
+// check its all adjacent pairs
         for (auto i:adj[front]){
             if(!visited[i]){
                 q.push(i);
-                visited[i = true];
+                visited[i] =  true;
             }
         }
      }
@@ -35,15 +43,17 @@ vector<int>solve(int n, int u, int v, int direction){
     unordered_map<int,bool>visited;
     vector<int>ans;
 
+    // create adjanceny list
     adj[u].push_back(v);
 
     if(direction == 0){
         adj[v].push_back(u);
     }
 
-   
+//    traverse all edges
 
     for(int i=0;i<n;i++){
+        // checking only for the edge which is not travers
         if(!visited[i]){
             bfs(adj,visited,ans,i);
         }
